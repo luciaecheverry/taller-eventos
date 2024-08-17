3080d44edd04b6d735f9cda65e172da5259f012c
LuciaEcheverry <echeverryferreyralucia@gmail.com>
2024-08-17 17:42:20 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -3,3 +3,10 @@ const div = document.getElementById("boton");
 div.addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
+
+function func1(event) {
+    alert("DIV 1");
+    if (document.getElementById("boton").checked) {
+      event.stopPropagation();
+    }
+  }
